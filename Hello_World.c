1dff9895f82a6ea4bb330728fa5711d1969a93dc
Shinjon Sharoj <eshikashinjonsharoj@gmail.com>
2025-10-01 00:33:40 +0600

    Add this

---
+++ Hello_World.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // header file 
+ 
+int main() // main function [execution starts here]
+{ //main function starts 
+ 
+    printf("Hello World!\n");
+    //printf function is called to show output 
+    return 0;
+} // main function ends
