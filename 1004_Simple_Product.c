5c7d670d6f1df8759125dfda97cbbd0aa788db6e
Shinjon Sharoj <eshikashinjonsharoj@gmail.com>
2025-10-02 03:13:37 +0600

    Add this

---
+++ 1004_Simple_Product.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // header file 
+ int main() // main function [execution starts from here] 
+{ // main function starts 
+ 
+    int PROD,x,y; // variable declaration
+    scanf("%d %d", &x, &y); // to read the input of integer data type 
+    PROD=x*y;
+    printf("PROD = %d\n", PROD); // printf is called to show output  
+    return 0;
+} // main function ends 
