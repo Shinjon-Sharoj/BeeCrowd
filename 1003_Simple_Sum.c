f3a9603a872e2b051069f02798097f768bb31e90
Shinjon Sharoj <eshikashinjonsharoj@gmail.com>
2025-10-02 03:10:00 +0600

    Add this

---
+++ 1003_Simple_Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>//header file
+ 
+int main()// main function [execution starts from here] 
+{ // main function starts 
+ 
+    int A,B; // declaration of variables 
+    scanf("%d\n %d\n", &A,&B);//to read the input of integer data type 
+    printf("SOMA = %d\n", A+B);// printf is called to show output 
+    return 0;
+}//main function ends 
