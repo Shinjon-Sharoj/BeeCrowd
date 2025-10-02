b3b076d3ce26c6879863523bc32f400f8b1e57aa
Shinjon Sharoj <eshikashinjonsharoj@gmail.com>
2025-10-03 02:13:52 +0600

    Add this

---
+++ 1007_Difference.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // header file 
+ 
+int main() // main function [execution starts from here] 
+{ // main function starts 
+ 
+    int A,B,C,D,Dif;
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    Dif=(A*B)-(C*D);
+    printf ("DIFERENCA = %d\n", Dif);
+    return 0;
+} // Main function ends
